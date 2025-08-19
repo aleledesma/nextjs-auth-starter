67a261bd58d1f5821bb6d9a949b7a6957a370d62
Ledesma Alejo <aledesma@johnsonacero.com>
2025-08-19 08:54:41 -0300

    add validations on login form

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,17 +4,33 @@ import { signIn } from "next-auth/react"
 import { useRouter } from "next/navigation"
 import { useState } from "react"
 import Link from "next/link"
+import { z } from "zod"
+import { loginFormSchema } from "@/zod/schemas/authSchemas"
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
 
 export default function LoginPage() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<z.infer<typeof loginFormSchema>>({
+    resolver: zodResolver(loginFormSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  })
   const router = useRouter()
   const [error, setError] = useState<string | null>(null)
+  const [loading, setLoading] = useState(false)
 
-  async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+  async function onSubmit(values: z.infer<typeof loginFormSchema>) {
     try {
-      event.preventDefault()
-      const formData = new FormData(event.currentTarget)
+      setLoading(true)
+      setError(null)
       const response = await signIn("credentials", {
-        ...Object.fromEntries(formData),
+        ...values,
         redirect: false,
       })
 
@@ -27,6 +43,8 @@ export default function LoginPage() {
       router.refresh()
     } catch {
       setError("An error occurred during login")
+    } finally {
+      setLoading(false)
     }
   }
 
@@ -38,7 +56,7 @@ export default function LoginPage() {
             Sign in to your account
           </h2>
         </div>
-        <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
+        <form className="mt-8 space-y-6" onSubmit={handleSubmit(onSubmit)}>
           <div className="flex flex-col gap-6">
             <div>
               <label
@@ -49,12 +67,16 @@ export default function LoginPage() {
               </label>
               <input
                 id="email"
-                name="email"
                 type="email"
-                required
                 className="mt-2 appearance-none relative block w-full px-3 py-2.5 border border-gray-300 placeholder-gray-400 text-gray-900 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
                 placeholder="Email address"
+                {...register("email")}
               />
+              {errors.email && (
+                <span className="text-sm text-red-500">
+                  {errors.email.message}
+                </span>
+              )}
             </div>
             <div>
               <label
@@ -65,21 +87,26 @@ export default function LoginPage() {
               </label>
               <input
                 id="password"
-                name="password"
                 type="password"
-                required
                 className="mt-2 appearance-none relative block w-full px-3 py-2.5 border border-gray-300 placeholder-gray-400 text-gray-900 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
                 placeholder="Password"
+                {...register("password")}
               />
+              {errors.password && (
+                <span className="text-sm text-red-500">
+                  {errors.password.message}
+                </span>
+              )}
             </div>
           </div>
 
           {error && (
-            <div className="text-red-500 text-sm text-center">{error}</div>
+            <div className="text-red-500 text-sm mt-3 text-center">{error}</div>
           )}
 
           <div>
             <button
+              disabled={loading}
               type="submit"
               className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
             >
